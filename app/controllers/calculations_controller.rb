b12965dd6d648761549538cb365535222ab279d6
awtauer <atauer2015@kellogg.northwestern.edu>
2014-05-10 15:24:57 -0500

    Boom Payment Method Done!
    Yipee

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,5 +10,12 @@ class CalculationsController < ApplicationController
   end
 
   def pmt
+    @interest_rate = params[:interest_rate].to_f/100
+    @number_of_payments = params[:number_of_payments].to_f
+    @present_value = params[:present_value].to_f
+    @payment = (@interest_rate*@number_of_payments)/(1-(1+@interest_rate)**(-1*@present_value))
   end
+
+    #"/payment/:interest_rate/:number_of_payments/:present_value
+
 end
